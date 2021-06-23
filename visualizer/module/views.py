fcc90ae20cd099d30b1f348dfe3b78dbd6ecdad0
Nicolas <nicolas.roger@epita.fr>
2021-06-23 15:38:48 +0200

    fix missing html files, refactor home

--- visualizer/module/views.py
+++ visualizer/module/views.py
@@ -13,6 +13,7 @@ from django.template.response import TemplateResponse
 from module.forms import Display
 from django.shortcuts import render
 from io import StringIO,BytesIO
+import base64
 
 def makeSlice(img_data, axis, slice_nb):
     if axis=="X":
@@ -31,13 +32,43 @@ def openFile(file_name, folder_path):
     img_data = nib.load(filepath).get_fdata()
     return img_data
 
-def makeHist(ax, data, file_name):
+def makeHist(ax, data, label):
     flatten = data.flatten()
     rescale = ((flatten + np.absolute(flatten.min()))/np.absolute(flatten.max())) * 255
     without_zeroes = rescale[np.nonzero(rescale)]
     ax.hist(without_zeroes, bins=np.arange(1, 255), histtype='step')
-    ax.set(xlabel=f"{file_name} color histogram")
+    ax.set(xlabel=label)
     return ax
+
+def drawSlice(axis, slice_nb, file_name):
+    img_data = openFile(file_name, settings.IMAGES_DATA_PATH)
+    if isinstance(img_data, HttpResponse):
+        return img_data
+
+    maximum_slice_nb = img_data.shape[{"X":0,"Y":1,"Z":2}[axis]] - 1
+    
+    if maximum_slice_nb < slice_nb:
+        return HttpResponse(f"<h1> Not enough slices in the requested axis \
+        ({axis}). Max is {maximum_slice_nb}. </h1>", status=405)
+
+    slice_data = makeSlice(img_data, axis, slice_nb)
+    fig,ax = plt.subplots()
+    ax.imshow(slice_data.T, cmap="gray", origin="lower")
+    ax.set(xlabel=f"slice {slice_nb} axis {axis}")
+    canvas = FigureCanvasAgg(fig)
+    return canvas
+
+def drawHist(file_name):
+    img_data = openFile(file_name, settings.IMAGES_DATA_PATH)
+    if isinstance(img_data, HttpResponse):
+        return img_data
+
+    fig,ax = plt.subplots()
+    ax = makeHist(ax, img_data, f"{file_name} grey scale histogram")
+
+    canvas = FigureCanvasAgg(fig)
+    return canvas
+
 
 @csrf_exempt
 def home(request):
@@ -57,37 +88,27 @@ def home(request):
                 display_form.cleaned_data["file_name"])
                 if isinstance(canvas, HttpResponse):
                     return canvas
+            elif func=="hist":
+                if not display_form.cleaned_data["use_mask"]:
+                    canvas = drawHist( 
+                    display_form.cleaned_data["file_name"])
+                    if isinstance(canvas, HttpResponse):
+                        return canvas
+                
+                else:
+                    canvas = drawMaskHist(
+                    display_form.cleaned_data["file_name"], 
+                    display_form.cleaned_data["file_name"])
+                    if isinstance(canvas, HttpResponse):
+                        return canvas
+            else:
+                return HttpResponse("<h1> Func do not exist </h1>",status=404)
 
-                canvas.print_png(graphic)
-
-            # print(display_form.cleaned_data["file_name"])
-            # print(display_form.cleaned_data["slice_nb"])
-            # print(display_form.cleaned_data["use_mask"])
-            
-            return render(request, "files_previewer.html", {"img": graphic, "files":files})
+            canvas.print_png(graphic)
+            encoded_graphic = base64.b64encode(graphic.getvalue())
+            return render(request, "files_previewer.html", {"img": str(encoded_graphic)[2:-1], "files":files})
 
     return render(request, "files_previewer.html", {"img": None, "files":files})
-
-
-def drawSlice(axis, slice_nb, file_name):
-    img_data = openFile(file_name, settings.IMAGES_DATA_PATH)
-    if isinstance(img_data, HttpResponse):
-        print("img data")
-        return img_data
-
-    maximum_slice_nb = img_data.shape[{"X":0,"Y":1,"Z":2}[axis]] - 1
-    
-    if maximum_slice_nb < slice_nb:
-        print("slice nb")
-        return HttpResponse(f"<h1> Not enough slices in the requested axis \
-        ({axis}). Max is {maximum_slice_nb}. </h1>", status=405)
-
-    slice_data = makeSlice(img_data, axis, slice_nb)
-    fig,ax = plt.subplots()
-    ax.imshow(slice_data.T, cmap="gray", origin="lower")
-    ax.set(xlabel=f"slice {slice_nb} axis {axis}")
-    canvas = FigureCanvasAgg(fig)
-    return canvas
 
 
 def getSlice(request, axis, slice_nb, file_name):
@@ -103,71 +124,21 @@ def getSlice(request, axis, slice_nb, file_name):
 
     return response
 
+
 def getHist(request, file_name):
     if request.method != "GET":
         return HttpResponse("<h1> Wrong request type</h1>",status=405)
 
-    img_data = openFile(file_name, settings.IMAGES_DATA_PATH)
-    if isinstance(img_data, HttpResponse):
-        return img_data
-
+   
+    canvas = drawHist(file_name)
+    if isinstance(canvas, HttpResponse):
+        return canvas
     
-    fig,ax = plt.subplots()
-    ax = makeHist(ax, img_data, file_name)
-
     response = HttpResponse(content_type = 'image/png')
-    canvas = FigureCanvasAgg(fig)
     canvas.print_png(response)
-    
     return response
 
-# def getSlice(request, axis, slice_nb, file_name):
-#     if request.method != "GET":
-#         return HttpResponse("<h1> Wrong request type </h1>",status=405)
-    
-#     slice_nb = int(slice_nb)
-#     img_data = openFile(file_name, settings.IMAGES_DATA_PATH)
-#     if isinstance(img_data, HttpResponse):
-#         return img_data
-
-#     maximum_slice_nb = img_data.shape[{"X":0,"Y":1,"Z":2}[axis]] - 1
-    
-#     if maximum_slice_nb < slice_nb:
-#         return HttpResponse(f"<h1> Not enough slices in the requested axis \
-#         ({axis}). Max is {maximum_slice_nb}. </h1>", status=405)
-
-#     slice_data = makeSlice(img_data, axis, slice_nb)
-#     fig,ax = plt.subplots()
-#     ax.imshow(slice_data.T, cmap="gray", origin="lower")
-#     ax.set(xlabel=f"slice {slice_nb} axis {axis}")
-
-#     response = HttpResponse(content_type = 'image/png')
-#     canvas = FigureCanvasAgg(fig)
-#     canvas.print_png(response)
-#     return response
-
-# def getHist(request, file_name):
-#     if request.method != "GET":
-#         return HttpResponse("<h1> Wrong request type</h1>",status=405)
-
-#     img_data = openFile(file_name, settings.IMAGES_DATA_PATH)
-#     if isinstance(img_data, HttpResponse):
-#         return img_data
-
-    
-#     fig,ax = plt.subplots()
-#     ax = makeHist(ax, img_data, file_name)
-
-#     response = HttpResponse(content_type = 'image/png')
-#     canvas = FigureCanvasAgg(fig)
-#     canvas.print_png(response)
-    
-#     return response
-
-def getMaskHist(request, file_name, mask_name):
-    if request.method != "GET":
-        return HttpResponse("<h1>Wrong request type<\h1>", status=405)
-
+def drawMaskHist(file_name, mask_name):
     img_data = openFile(file_name, settings.IMAGES_DATA_PATH)
     if isinstance(img_data, HttpResponse):
         return img_data
@@ -183,9 +154,17 @@ def getMaskHist(request, file_name, mask_name):
     mask_data = mask_data.astype(bool)
     masked_data = img_data[mask_data] 
     fig,ax = plt.subplots()
-    ax = makeHist(ax, masked_data, file_name)
-
-    response = HttpResponse(content_type = 'image/png')
+    ax = makeHist(ax, masked_data, f"{file_name} masked grey scale histogram")    
     canvas = FigureCanvasAgg(fig)
+    return canvas
+
+def getMaskHist(request, file_name, mask_name):
+    if request.method != "GET":
+        return HttpResponse("<h1>Wrong request type<\h1>", status=405)
+
+    canvas = drawMaskHist(file_name, mask_name)
+    if isinstance(canvas, HttpResponse):
+        return canvas
+    response = HttpResponse(content_type = 'image/png')
     canvas.print_png(response)
     return response
